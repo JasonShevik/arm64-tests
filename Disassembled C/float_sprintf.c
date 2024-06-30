f68407d2f273c86c8ab55bd5a0d4051e406bf294
JasonShevik <jasonshevik@gmail.com>
2024-06-30 16:37:08 -0500

    Replaced the float to string program with float_sprintf using the sprintf function instead of snprintf to keep the assembly simpler.

--- Disassembled C/float_sprintf.c
+++ Disassembled C/float_sprintf.c
@@ -4,7 +4,7 @@ int main() {
 	float num = 3.14;
 	char str[20];
 	
-	snprintf(str, sizeof(str), "%f", num);
+	sprintf(str, "%f", num);
 	printf("%s\n", str);
     
     return 0;
--- Disassembled C/float_sprintf.s
+++ Disassembled C/float_sprintf.s
@@ -1,22 +1,22 @@
 
-float_string:     file format elf64-littleaarch64
+float_sprintf:     file format elf64-littleaarch64
 
 
 Disassembly of section .init:
 
-00000000000005f8 <_init>:
- 5f8:	d503201f 	nop
- 5fc:	a9bf7bfd 	stp	x29, x30, [sp, #-16]!
- 600:	910003fd 	mov	x29, sp
- 604:	9400003c 	bl	6f4 <call_weak_fn>
- 608:	a8c17bfd 	ldp	x29, x30, [sp], #16
- 60c:	d65f03c0 	ret
+00000000000005f0 <_init>:
+ 5f0:	d503201f 	nop
+ 5f4:	a9bf7bfd 	stp	x29, x30, [sp, #-16]!
+ 5f8:	910003fd 	mov	x29, sp
+ 5fc:	9400003e 	bl	6f4 <call_weak_fn>
+ 600:	a8c17bfd 	ldp	x29, x30, [sp], #16
+ 604:	d65f03c0 	ret
 
 Disassembly of section .plt:
 
 0000000000000610 <.plt>:
  610:	a9bf7bf0 	stp	x16, x30, [sp, #-16]!
- 614:	f00000f0 	adrp	x16, 1f000 <__FRAME_END__+0x1e6d8>
+ 614:	f00000f0 	adrp	x16, 1f000 <__FRAME_END__+0x1e6e0>
  618:	f947fe11 	ldr	x17, [x16, #4088]
  61c:	913fe210 	add	x16, x16, #0xff8
  620:	d61f0220 	br	x17
@@ -36,7 +36,7 @@ Disassembly of section .plt:
  648:	91002210 	add	x16, x16, #0x8
  64c:	d61f0220 	br	x17
 
-0000000000000650 <snprintf@plt>:
+0000000000000650 <sprintf@plt>:
  650:	90000110 	adrp	x16, 20000 <__libc_start_main@GLIBC_2.34>
  654:	f9400a11 	ldr	x17, [x16, #16]
  658:	91004210 	add	x16, x16, #0x10
@@ -70,7 +70,7 @@ Disassembly of section .text:
  6d0:	f94003e1 	ldr	x1, [sp]
  6d4:	910023e2 	add	x2, sp, #0x8
  6d8:	910003e6 	mov	x6, sp
- 6dc:	f00000e0 	adrp	x0, 1f000 <__FRAME_END__+0x1e6d8>
+ 6dc:	f00000e0 	adrp	x0, 1f000 <__FRAME_END__+0x1e6e0>
  6e0:	f947ec00 	ldr	x0, [x0, #4056]
  6e4:	d2800003 	mov	x3, #0x0                   	// #0
  6e8:	d2800004 	mov	x4, #0x0                   	// #0
@@ -78,7 +78,7 @@ Disassembly of section .text:
  6f0:	97ffffe0 	bl	670 <abort@plt>
 
 00000000000006f4 <call_weak_fn>:
- 6f4:	f00000e0 	adrp	x0, 1f000 <__FRAME_END__+0x1e6d8>
+ 6f4:	f00000e0 	adrp	x0, 1f000 <__FRAME_END__+0x1e6e0>
  6f8:	f947e800 	ldr	x0, [x0, #4048]
  6fc:	b4000040 	cbz	x0, 704 <call_weak_fn+0x10>
  700:	17ffffd8 	b	660 <__gmon_start__@plt>
@@ -93,7 +93,7 @@ Disassembly of section .text:
  71c:	91010021 	add	x1, x1, #0x40
  720:	eb00003f 	cmp	x1, x0
  724:	540000c0 	b.eq	73c <deregister_tm_clones+0x2c>  // b.none
- 728:	f00000e1 	adrp	x1, 1f000 <__FRAME_END__+0x1e6d8>
+ 728:	f00000e1 	adrp	x1, 1f000 <__FRAME_END__+0x1e6e0>
  72c:	f947e021 	ldr	x1, [x1, #4032]
  730:	b4000061 	cbz	x1, 73c <deregister_tm_clones+0x2c>
  734:	aa0103f0 	mov	x16, x1
@@ -110,7 +110,7 @@ Disassembly of section .text:
  758:	8b810c41 	add	x1, x2, x1, asr #3
  75c:	9341fc21 	asr	x1, x1, #1
  760:	b40000c1 	cbz	x1, 778 <register_tm_clones+0x38>
- 764:	f00000e2 	adrp	x2, 1f000 <__FRAME_END__+0x1e6d8>
+ 764:	f00000e2 	adrp	x2, 1f000 <__FRAME_END__+0x1e6e0>
  768:	f947f042 	ldr	x2, [x2, #4064]
  76c:	b4000062 	cbz	x2, 778 <register_tm_clones+0x38>
  770:	aa0203f0 	mov	x16, x2
@@ -125,7 +125,7 @@ Disassembly of section .text:
  78c:	90000113 	adrp	x19, 20000 <__libc_start_main@GLIBC_2.34>
  790:	39410260 	ldrb	w0, [x19, #64]
  794:	35000140 	cbnz	w0, 7bc <__do_global_dtors_aux+0x3c>
- 798:	f00000e0 	adrp	x0, 1f000 <__FRAME_END__+0x1e6d8>
+ 798:	f00000e0 	adrp	x0, 1f000 <__FRAME_END__+0x1e6e0>
  79c:	f947e400 	ldr	x0, [x0, #4040]
  7a0:	b4000080 	cbz	x0, 7b0 <__do_global_dtors_aux+0x30>
  7a4:	90000100 	adrp	x0, 20000 <__libc_start_main@GLIBC_2.34>
@@ -152,23 +152,22 @@ Disassembly of section .text:
  7e8:	bd002fe0 	str	s0, [sp, #44]
  7ec:	bd402fe0 	ldr	s0, [sp, #44]
  7f0:	1e22c000 	fcvt	d0, s0
- 7f4:	910063e3 	add	x3, sp, #0x18
+ 7f4:	910063e2 	add	x2, sp, #0x18
  7f8:	90000000 	adrp	x0, 0 <__abi_tag-0x278>
- 7fc:	91210002 	add	x2, x0, #0x840
- 800:	d2800281 	mov	x1, #0x14                  	// #20
- 804:	aa0303e0 	mov	x0, x3
- 808:	97ffff92 	bl	650 <snprintf@plt>
- 80c:	910063e0 	add	x0, sp, #0x18
- 810:	97ffff9c 	bl	680 <puts@plt>
- 814:	52800000 	mov	w0, #0x0                   	// #0
- 818:	a8c37bfd 	ldp	x29, x30, [sp], #48
- 81c:	d65f03c0 	ret
+ 7fc:	9120e001 	add	x1, x0, #0x838
+ 800:	aa0203e0 	mov	x0, x2
+ 804:	97ffff93 	bl	650 <sprintf@plt>
+ 808:	910063e0 	add	x0, sp, #0x18
+ 80c:	97ffff9d 	bl	680 <puts@plt>
+ 810:	52800000 	mov	w0, #0x0                   	// #0
+ 814:	a8c37bfd 	ldp	x29, x30, [sp], #48
+ 818:	d65f03c0 	ret
 
 Disassembly of section .fini:
 
-0000000000000820 <_fini>:
- 820:	d503201f 	nop
- 824:	a9bf7bfd 	stp	x29, x30, [sp, #-16]!
- 828:	910003fd 	mov	x29, sp
- 82c:	a8c17bfd 	ldp	x29, x30, [sp], #16
- 830:	d65f03c0 	ret
+000000000000081c <_fini>:
+ 81c:	d503201f 	nop
+ 820:	a9bf7bfd 	stp	x29, x30, [sp, #-16]!
+ 824:	910003fd 	mov	x29, sp
+ 828:	a8c17bfd 	ldp	x29, x30, [sp], #16
+ 82c:	d65f03c0 	ret
