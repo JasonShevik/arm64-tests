b24ebd7efa98f84c1c17d4ca6a0ca1f48defa4f3
JasonShevik <jasonshevik@gmail.com>
2024-07-06 00:44:45 -0500

    Added a file called int_string_basic.c with just a basic outline on my plan to convert an integer to a string in C manually with no libraries. This will help me implement something similar for my compound_interst.s program. I have an idea on how to implement it in ARM64 already, but I'd like to compare to the results of this.

---
+++ Disassembled C/int_string_basic.c
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+int main() {
+	int money = 12900;
+	
+	// I'll make a counter
+	
+	// Then repeatedly mod money by 10
+	// Each time checking if the result is greater than 10
+	// Incrementing the counter as necessary
+	for() {
+		
+		
+		
+	}
+	
+	// Then once I have the length of the integer
+	// I will allocate space for that many chars plus null terminator
+	// I can exponent 10 by length-1 and repeatedly mod by that
+	// Adding the remainder to the string using ASCII math
+	// and subtracting from money until I get to 0
+	// add null terminator
+	
+	
+	
+	return 0;
+}
